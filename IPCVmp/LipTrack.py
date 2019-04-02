b404689776baa36a1fdbd05ebc571f51a9bf6c8e
Andreea Ciontos <andreeaciontos1@gmail.com>
2019-04-02 17:00:48 +0200

    Liptracking code based on face track

--- IPCVmp/LipTrack.py
+++ IPCVmp/LipTrack.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
-
+import matplotlib.pyplot as plt
+from skimage.color import rgb2gray
+from skimage import data
+from skimage.filters import gaussian
+from skimage.segmentation import active_contour
 
 faceDetection = cv2.CascadeClassifier('D:\opencv\sources\data\haarcascades\haarcascade_frontalface_default.xml')
    
@@ -22,6 +26,21 @@ while 1:
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
+
+s = np.linspace(0, 2*np.pi, 400)
+x = 220 + 100*np.cos(s)
+y = 100 + 100*np.sin(s)
+init = np.array([x, y]).T
+
+snake = active_contour(gaussian(img, 3),
+                       init, alpha=0.015, beta=10, gamma=0.001)
+
+fig, ax = plt.subplots(figsize=(7, 7))
+ax.imshow(img, cmap=plt.cm.gray)
+ax.plot(init[:, 0], init[:, 1], '--r', lw=3)
+ax.plot(snake[:, 0], snake[:, 1], '-b', lw=3)
+ax.set_xticks([]), ax.set_yticks([])
+ax.axis([0, img.shape[1], img.shape[0], 0])
 
 VideoFeed.release()
 cv2.destroyAllWindows()
